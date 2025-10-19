1659f9ea1f222db60f1a0d3d3991d083f8dd9652
Mobeenkhxn01 <mobeenkhan191915@gmail.com>
2025-10-19 23:49:37 +0530

    stack and heap concepts

--- 01_basics/05_datatype-summary.js
+++ 01_basics/05_datatype-summary.js
@@ -28,17 +28,50 @@
 // 3. Functions
 
 
-const heros=["shaktiman","naagraj","doga"];
-let myObj={
-    name:"Mobeen",
-    age:22,
-    isLoggedIn:false
+// const heros=["shaktiman","naagraj","doga"];
+// let myObj={
+//     name:"Mobeen",
+//     age:22,
+//     isLoggedIn:false
+// }
+
+// const myfunction= function(){
+//     console.log("Hello World");
+// }
+
+// console.log(typeof heros); // object
+// console.log(typeof myObj); // object
+// console.log(typeof myfunction); // function
+
+
+// ******** Stack (Primitive) and Heap (Non-Primitive) Memory ********
+let myname= "mobeenkhan";
+let anotherName=myname;
+
+console.log(anotherName); // mobeenkhan
+anotherName="khanmobeen";
+
+console.log(myname); // mobeenkhan
+console.log(anotherName); // 
+
+
+let userOne={
+    email:"mobeen@example.com",
+    upi:"mobeen@oksbi",
 }
 
-const myfunction= function(){
-    console.log("Hello World");
-}
+let userTwo=userOne;
+console.log(userTwo);
 
-console.log(typeof heros); // object
-console.log(typeof myObj); // object
-console.log(typeof myfunction); // function
+/*
+Stack Memory:
+It uses to store primitive data types (Number, String, Boolean, Undefined, Null, Symbol, BigInt).
+and it stores the actual values.
+It sends a copy of the value when assigned to another variable.
+
+Heap Memory:
+It uses to store non-primitive data types (Object, Array, Functions).
+It stores the reference(address) of the object.
+It sends the reference(address) when assigned to another variable.
+
+*/
