fad91b4d713220ede5919cb822db2258a3fe7f4e
Mobeenkhxn01 <mobeenkhan191915@gmail.com>
2025-10-19 23:18:44 +0530

    conbersionOperations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -10,9 +10,9 @@
 
 // "33abc" => NaN ( Not a Number )
 
-let isLoggedIn="mobeen";
-let booleanIsLoggedIn=Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let isLoggedIn="mobeen";
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // "" => false
 // 0 => false
@@ -21,10 +21,57 @@ console.log(booleanIsLoggedIn);
 // null => false
 // undefined => false
 
-let someNumber=33;
-let stringNumber=String(someNumber);
-console.log(typeof stringNumber);
+// let someNumber=33;
+// let stringNumber=String(someNumber);
+// console.log(typeof stringNumber);
 // 33 => "33"
+
+
+
+// ********** Conversion operation in JS ********** //
+
+// let value=3
+// let negValue=-value;
+
+// console.log(negValue);
+
+// let str1="hello";
+// let str2=" world";
+// let str3=str1+str2;
+
+//console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);  
+// console.log(1+2+"2");
+
+// If any one operand is string then other will be converted to string and concatenation will be performed.
+// Otherwise addition will be performed.
+// + operator is used for both addition and concatenation.
+
+
+// console.log(+true); // 1
+// console.log(+false); // 0
+
+// let num1,num2,num3;
+// num1=num2=num3=2+2;
+// console.log(num1,num2,num3);
+// Associativity from right to left 
+// num3=2+2 =>4
+// num2=num3 =>4
+// num1=num2 =>4
+
+
+// let gameCounter=100;
+// gameCounter++;
+// console.log(gameCounter);
+ //post increment operator
+
+// ++gameCounter;
+// console.log(gameCounter);
+// pre increment operator
+
 
 
 
